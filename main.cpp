85c2d9cf4c60c36cb06cb411cc3e8bead17c1777
edbird <edbird@users.noreply.github.com>
2018-12-06 16:13:08 +0000

    removed code from main

--- main.cpp
+++ main.cpp
@@ -16,37 +16,6 @@
 #include <vector>
 
 
-class ErrorLog
-{
-
-    public:
-
-    ErrorLog(const std::string& filename)
-    {
-        error_log.open(filename.c_str(), std::ios::trunc);
-    }
-
-
-    virtual
-    ~ErrorLog()
-    {
-        error_log.flush();
-        error_log.close();
-    }
-
-
-    void Print(const std::string& message)
-    {
-        error_log << message << std::endl;
-    }
-
-
-
-    protected:
-
-    std::ofstream error_log;
-
-};
 
 
 
